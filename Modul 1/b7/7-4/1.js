16bcf315e6cb8a04fce6ebc98b7654993aa46cd1
vuquangtu <quangtu.vu@gmail.com>
2023-11-07 14:46:31 +0700

    add all file modul

---
+++ Modul 1/b7/7-4/1.js
@@ -0,0 +1,12 @@
+var rong;
+var cao;
+rong = prompt("nhap chieu rong");
+cao = prompt("nhap chieu cao");
+// document.write("dien tich hinh chu nhat la = " + rong);
+// document.write("dien tich hinh chu nhat la = " + cao);
+var width = parseInt(rong);
+var height = parseInt(cao);34
+var area = width * height;3532
+document.write("dien tich hinh chu nhat la = " + area);
+
+
